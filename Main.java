3f6409e59d91781c7539b37fe663c5f6c1b7a1a6
Matthew Guizar <matthewrguizar@gmail.com>
2022-10-02 05:46:53 -0700

    more regex matchers

--- Main.java
+++ Main.java
@@ -20,5 +20,25 @@ public class Main {
         //removed if one of the letters from the first brackets are followed by any of the second letters
         System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
         
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+        String newAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+
+        //replaces all characters not in the brackets
+        System.out.println(newAlphanumeric.replaceAll("[^ej]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[abcdef345678]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[a-fA-F3-8]", "X"));
+
+        //ignore case(case insensitivity)
+        System.out.println(newAlphanumeric.replaceAll("(?i)[a-f3-9]", "X"));  
+
+
+        //both replace 0-9
+        System.out.println(newAlphanumeric.replaceAll("[0-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\d", "X"));
+
+        //replaces all non digits
+        System.out.println(newAlphanumeric.replaceAll("\\D", "X"));
+
+       
     }
 }
